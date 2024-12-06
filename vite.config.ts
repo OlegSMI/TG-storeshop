958661592f3abdab188a011f0060ecfdc9fe0af8
Oleg Maleg <slovplan@mail.ru>
2024-12-06 20:11:35 +0300

    Edit bot sertificates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import vue from "@vitejs/plugin-vue";
+import * as fs from "fs";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync("192.168.0.157-key.pem"),
+      cert: fs.readFileSync("192.168.0.157.pem"),
+    },
+    // host: import.meta.env.VITE_IP,
+    host: "192.168.0.157",
+    port: 3000,
+    open: true, // Автоматически открывать браузер при запуске
+  },
   // resolve: {
   //   alias: {
   //     "@": path.resolve(__dirname, "./src"),
