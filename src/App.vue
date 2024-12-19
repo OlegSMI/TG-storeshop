4aa06a3529a8eb9b83affb2c32a1789eeb3583f0
Oleg Maleg <slovplan@mail.ru>
2024-12-19 10:58:27 +0300

    add message

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,12 @@ const { onThemeChanged } = useWebAppTheme();
 onThemeChanged(
   () => (mainButton.color = colorScheme.value == "dark" ? "#3e88f7" : "#007aff")
 );
+
+window.addEventListener("message", (event) => {
+  console.log(event);
+  const appId = event.data;
+  console.log("Получен app_id:", appId);
+});
 </script>
 
 <template>
